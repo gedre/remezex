701431f691448b299f4fc2b38f87b9ea3b871b1f
Georg Drenkhahn <Georg.Drenkhahn+github@mailbox.org>
2024-01-01 22:20:40 +0100

    changes output format to html

--- build.m
+++ build.m
@@ -1,6 +1,6 @@
 % export files to pdf format
-export("remezex.mlx",      OpenExportedFile=false, Run=false);
-export("remezex_demo.mlx", OpenExportedFile=false, Run=false);
+export("remezex.mlx",      Format="html", OpenExportedFile=false, Run=false);
+export("remezex_demo.mlx", Format="html", OpenExportedFile=false, Run=false);
 
 % build archive
-zip("remezex.zip", ["*.mlx", "*.pdf", "README.md", "LICENSE"]);
+zip("remezex.zip", ["*.mlx", "*.html", "README.md", "LICENSE"]);
