c500ab70e519a6122f116671db53486b17fc6240
Georg Drenkhahn <Georg.Drenkhahn+github@mailbox.org>
2024-01-01 22:32:50 +0100

    check in of html artifacts

--- build.m
+++ build.m
@@ -1,6 +1,15 @@
-% export files to pdf format
-export("remezex.mlx",      Format="html", OpenExportedFile=false, Run=false);
-export("remezex_demo.mlx", Format="html", OpenExportedFile=false, Run=false);
+% execute the demo
+remezex_demo;
+close all;
+
+%% export mlx files
+% the export() command causes an error in GitHub Actions:
+%   "Error using matlab.internal.cef.webwindow, DISPLAY environment variable must be set to a valid X11 display."
+% Therefore the html pages need to be checked in.
+if ispc
+    export("remezex.mlx",      Format="html", OpenExportedFile=false, Run=false);
+    export("remezex_demo.mlx", Format="html", OpenExportedFile=false, Run=false);
+end
 
 % build archive
 zip("remezex.zip", ["*.mlx", "*.html", "README.md", "LICENSE"]);
