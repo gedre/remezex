9548abbf1d89ffb71fb5a869117d894bc4bc2ef3
Georg Drenkhahn <Georg.Drenkhahn+github@mailbox.org>
2024-08-02 17:27:31 +0200

    update to newest GitHub Actions, typos, removing redundant html pages

--- build.m
+++ build.m
@@ -3,10 +3,11 @@ remezex_demo;
 close all;
 
 %% export mlx files
-% the export() command causes an error in GitHub Actions:
+% the export() command causes an error in GitHub Actions running in an Linux environment:
 %   "Error using matlab.internal.cef.webwindow, DISPLAY environment variable must be set to a valid X11 display."
-% Therefore the html pages need to be checked in.
-if ispc
-    export("remezex.mlx",      Format="html", OpenExportedFile=false, Run=false);
-    export("remezex_demo.mlx", Format="html", OpenExportedFile=false, Run=false);
-end
+% Deactivated for now.
+%if ispc
+%    cd("html");
+%    export("../remezex.mlx",      Format="html", OpenExportedFile=false, Run=false);
+%    export("../remezex_demo.mlx", Format="html", OpenExportedFile=false, Run=false);
+%end
